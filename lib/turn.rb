b9fcb6e21b3508a8d0283ecec7777ff6f233cd71
Nichole Battleday <battleday@gmail.com>
2019-05-16 03:14:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,10 @@ ef display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def turn(board)
+  puts "Please enter 1-9:"
+end
+
 def input_to_index(user_input)
   new_user_input = user_input.to_i
   new_user_input -= 1
@@ -17,6 +21,26 @@ def move(board, index, character = "X")
   return board
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
 end
